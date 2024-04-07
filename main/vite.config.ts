4188d5268bd814c264c74bfd1164cbf363a39d51
geumbo <82752974+hello12324@users.noreply.github.com>
2024-04-07 18:28:11 +0900

    setup page

---
+++ main/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url';
+// 如果您的GitHub仓库名是`birthday`，并且项目托管在`https://<username>.github.io/birthday/`
+const repoName = 'KernelKraze'; // 请替换成您的仓库名
+
+export default defineConfig({
+  base: `/${repoName}/`, // 添加 base 配置，用于GitHub Pages服务的路径
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  // 其他配置...
+})
